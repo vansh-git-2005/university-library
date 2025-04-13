64266f8fa46f99a47381356cccdd44e4cdcdcdde
vansh-git-2005 <patelvansh0606@gmail.com>
2025-04-14 02:58:16 +0530

    resend error fix

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -26,7 +26,7 @@ export const sendEmail = async ({
       provider: resend({ token: config.env.resendToken }),
     },
     body: {
-      from: "JS Mastery <contact@adrianjsmastery.com>",
+      from: "Bookwise <contact@patelvansh.space>",
       to: [email],
       subject,
       html: message,
