4facf66a11d45868e57d02ef6669ee64f62a5595
vansh-git-2005 <patelvansh0606@gmail.com>
2025-04-14 02:07:50 +0530

    third

--- database/seed.ts
+++ database/seed.ts
@@ -5,7 +5,7 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import { config } from "dotenv";
 
-config({ path: ".env.local" });
+config({ path: ".env" });
 
 const sql = neon(process.env.DB_URL!);
 export const db = drizzle({ client: sql });
